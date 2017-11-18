bccb29227c62f79057b9b981ca8b30e929be405e
LeonGr <leongrasmeijer@gmail.com>
2017-11-18 13:10:24 +0100

    Add response to url change, add basic formatting

--- src/components/body/artist-view.vue
+++ src/components/body/artist-view.vue
@@ -4,57 +4,92 @@
 
 <template>
     <pre id="output">
-Artist: {{ artist }}
+		Artist: {{ artist }}
+
+        <div v-for="event in events" :key="event.datetime">
+            {{ event.datetime }}
+            {{ event.venue.name }}
+            {{ event.venue.city }}
+            {{ event.venue.country }}
+            <a :href="event.ticketUrl">Tickets</a>
+        </div>
     </pre>
 </template>
 
 <script>
+//TODO: Fix ticket output when there are no offers
 export default {
     data: function() {
         return {
-            artist: ''
+            artist: '',
+            events: {},
+        }
+    },
+
+    watch: {
+        '$route' () {
+            this.getArtistEvents();
         }
     },
 
     mounted: function() {
-        this.artist = this.$route.params.artist
+        this.getArtistEvents();
+    },
 
-        console.log("Initialize");
+    methods: {
+        getArtistEvents: function() {
+            this.artist = this.$route.params.artist
 
-        const apiURL = "https://rest.bandsintown.com/"
-        const apiExtension = "?app_id='ConcerTrack v0.0.1'"
+            const apiURL = "https://rest.bandsintown.com/"
+            const apiExtension = "?app_id='ConcerTrack v0.0.1'"
+            const output = document.getElementById("output");
 
-        const output = document.getElementById("output");
+            let getEvents = (artist) => {
+                return new Promise((resolve, reject) => {
+                    fetch(apiURL + "artists/" + artist + "/events" + apiExtension, {
+                        method: 'GET',
+                        headers: {
+                            'accept': "application/json"
+                        }
+                    }).then(response => {
+                        return response.json()
+                    }).then(response => {
+                        resolve(response);
+                    }).catch(error => {
+                        console.log(error);
+                    })
+                })
+            }
 
-        let getEvents = () => {
-            return new Promise((resolve, reject) => {
-                fetch(apiURL + "artists/" + this.artist + "/events" + apiExtension, {
-                    method: 'GET',
-                    headers: {
-                        'accept': "application/json"
+            getEvents(this.artist).then(data => {
+                this.events = data;
+                this.events.forEach((event) => {
+					console.log(event);
+                    if(event.offers.length){
+                        event.ticketUrl = event.offers[0].url;
                     }
-                }).then(response => {
-                    return response.json()
-                }).then(response => {
-                    resolve(response);
-                }).catch(error => {
-                    console.log(error);
+
+                    let date = new Date(event.datetime);
+                    let months = ['Jan','Feb','Mar','Apr','May',
+'June','July','Aug','Sept','Oct','Nov','Dec'];
+                    event.datetime = `${date.getDate()}-${months[date.getMonth()]}-${date.getFullYear()}`;
                 })
             })
-        }
 
-        let getEventsHere = () => {
-            getEvents().then(response => {
-                response.forEach(event => {
-                    if(event.venue.country == "Netherlands") {
-                        console.log(event);
-                        output.innerHTML += JSON.stringify(event, undefined, 4);
-                    }
+
+            let getEventsHere = () => {
+                getEvents().then(response => {
+//                    response.forEach(event => {
+//                        if(event.venue.country == "Netherlands") {
+//                            console.log(event);
+//                            output.innerHTML += JSON.stringify(event, undefined, 4);
+//                        }
+//                    })
                 })
-            })
-        }
+            }
 
-        getEventsHere();
+//            getEventsHere();
+        }
     }
 }
 </script>
